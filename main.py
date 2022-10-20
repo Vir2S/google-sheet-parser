34a16d3d2bdab75ba6e61cc2896bbf425953220b
vsem <virtualik@gmail.com>
2022-10-20 22:11:54 +0200

    json from remote csv functionality added

--- main.py
+++ main.py
@@ -1,17 +1,37 @@
+import json
 import pandas as pd
 
 URL = "https://drive.google.com/file/d/1zLdEcpzCp357s3Rse112Lch9EMUWzMLE/view"
 PATH = f"https://drive.google.com/uc?id={URL.split('/')[-2]}"
 
 
-def read_csv_from_remote_storage():
-    df = pd.read_csv(PATH)
-    print(df.values, len(df.values))
-    return df.values
+def write_json(file: dict, filename: str) -> None:
+    with open(filename, "w") as f:
+        f.write(json.dumps(file, indent=4))
+
+
+def create_json(dataframe=None) -> dict:
+    res = []
+    if dataframe is None:
+        return {
+            "data": res
+        }
+
+    for index, row in dataframe.iterrows():
+        res.append(dict(row))
+    return {
+        "data": res
+    }
+
+
+def read_csv_from_remote_storage(path: str = PATH, columns: list = None):
+    return pd.read_csv(path, usecols=columns)
 
 
 def main():
-    print(read_csv_from_remote_storage())
+    df = read_csv_from_remote_storage(path=PATH)
+    json_file = create_json(dataframe=df)
+    write_json(file=json_file, filename="out.json")
 
 
 if __name__ == "__main__":
