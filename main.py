169149d54a84fb27d44ef0a27a9cd6098344c580
vsem <virtualik@gmail.com>
2022-10-21 13:20:29 +0200

    argument parser added

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import pandas as pd
 
@@ -27,8 +28,34 @@ def read_csv_from_remote_storage(path: str = PATH, columns: list = None):
     return pd.read_csv(path, usecols=columns)
 
 
+def get_columns_list_from_parsed_arguments(arguments):
+    try:
+        columns = arguments.fields.split(",")
+    except Exception:
+        columns = None
+
+    return columns
+
+
+def parse_arguments():
+    arguments_required = {
+        "--fields": "required fields from csv",
+    }
+    parser = argparse.ArgumentParser(prog="main.py", add_help=True)
+
+    for key, value in arguments_required.items():
+        parser.add_argument(key, type=str.lower, help=value, required=False)
+
+    return parser.parse_args()
+
+
 def main():
-    df = read_csv_from_remote_storage(path=PATH)
+    arguments = parse_arguments()
+    columns = get_columns_list_from_parsed_arguments(arguments)
+
+    print(columns, type(columns))
+
+    df = read_csv_from_remote_storage(path=PATH, columns=columns)
     json_file = create_json(dataframe=df)
     write_json(file=json_file, filename="out.json")
 
