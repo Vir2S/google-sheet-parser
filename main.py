d3b8d1d07842d4c4ea4bcc55c83b79ed1b3b2410
vsem <virtualik@gmail.com>
2022-10-25 20:43:11 +0200

    get file name added

--- main.py
+++ main.py
@@ -15,6 +15,25 @@ def get_file_list(dir_name: str = DIR_NAME) -> list:
     return sorted(files)
 
 
+def get_file_name(files: list = None, filename: str = None) -> str:
+    if filename is None:
+        filename = FILENAME
+
+    if files is None or len(files) == 0:
+        files = [filename]
+
+    if len(files) == 1:
+        filename = str(files[-1].split(".")[-2] + f"-{1}" + "." + files[-1].split(".")[-1])
+    else:
+        try:
+            filename = f'{files[-1].split(".")[-2].split("-")[-2]}-{int(files[-1].split(".")[-2].split("-")[-1])+1}.{files[-1].split(".")[-1]}'
+        except IndexError:
+            filename = f'{files[-2].split(".")[-2].split("-")[-2]}-{int(files[-2].split(".")[-2].split("-")[-1])+1}.{files[-2].split(".")[-1]}'
+            # filename = filename
+
+    return filename
+
+
 def save_json_file(file: dict, filename: str = None, dir_name: str = DIR_NAME) -> None:
     if filename is None:
         filename = FILENAME
@@ -52,7 +71,7 @@ def get_columns_list_from_parsed_arguments(arguments) -> list or None:
 def parse_arguments():
     arguments_required = {
         "-fields": "required fields from csv",
-        "-filename": "full path to file with filename and ext"
+        "-filename": "filename with ext"
     }
     parser = argparse.ArgumentParser(prog="main.py", add_help=True)
 
@@ -64,12 +83,16 @@ def parse_arguments():
 
 def main():
 
-    print(get_file_list())
     arguments = parse_arguments()
+    print(get_file_list())
+    files = get_file_list()
+    print(get_file_name(files=files))
+    filename = arguments.filename or get_file_name(files=files)
+    print(filename)
     columns = get_columns_list_from_parsed_arguments(arguments=arguments.fields)
     df = read_csv_from_remote_storage(path=PATH, columns=columns)
     json_file = create_json_file(dataframe=df)
-    save_json_file(file=json_file, filename=arguments.filename)
+    save_json_file(file=json_file, filename=filename)
 
 
 if __name__ == "__main__":
