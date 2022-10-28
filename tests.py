b5a119f3ac749bafc8fc0cb31acfc3b745e1a412
vsem <virtualik@gmail.com>
2022-10-28 18:48:33 +0200

    tests extended

--- tests.py
+++ tests.py
@@ -25,3 +25,18 @@ def test_get_columns_list_from_parsed_arguments_with_list():
 def test_get_columns_list_from_parsed_arguments_with_tuple():
     arguments = ("clicks", "date")
     assert main.get_columns_list_from_parsed_arguments(arguments) is None
+
+
+@pytest.mark.create_json_file
+def test_create_json_file():
+    dataframe = None
+    assert main.create_json_file(dataframe) == {"data": []}
+
+
+@pytest.mark.create_json_file
+def test_create_json_file_with_data():
+    dataframe = main.pd.read_csv("samples/sample.csv")
+    with open("samples/sample.json", "r") as f:
+        json_sample = main.json.loads(f.read()).get("data")
+    assert main.create_json_file(dataframe) == {"data": json_sample}
+
